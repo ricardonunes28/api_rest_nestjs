ecb5c2f2b6bb565c3b2f110c9bc08b31f2d34add
Ricardo Nascimento <ricardo.nascimento@lobalhss.com.br,git>
2023-06-07 15:35:05 -0300

    [API_NESTJS_PG] * add winston for logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,25 @@ import { ConfigModule } from '@nestjs/config';
 import { typeOrmConfig } from './configs/typeorm.config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { WinstonModule } from 'nest-winston';
+import { winstonConfig } from './configs/winston.config';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { LoggerInterceptor } from './interceptors/logger.interceptor';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot(typeOrmConfig),
+    WinstonModule.forRoot(winstonConfig),
     UsersModule,
     AuthModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggerInterceptor,
+    },
+  ],
 })
 export class AppModule {}
